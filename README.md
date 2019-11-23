1d79b8c231ef546caa1df26406020b4657d85e06
Esther <esther.namusisi@andela.com>
2019-11-23 14:33:48 +0300

    display all data of all presidents of Uganda

---
+++ README.md
@@ -0,0 +1,17 @@
+# ugandan-presidents
+An npm package that returns all the presidents of Uganda
+
+## Installing
+`npm install`
+
+## Example
+Performing a request
+
+```
+  const presidents = require('ugandan-presidents');
+
+  console.log(presidents);
+
+```
+## Contributors
+Esther
---
+++ src/index.js
@@ -0,0 +1,58 @@
+module.exports = [
+  {
+  title: 'Sir',
+  name: 'Edward Luwangula Walugembe Muteesa II',
+  startDate: '9 Oct 1963',
+  endDate: '24 Feb 1966',
+  term:'first'
+  },
+  {
+    title: 'His Excellency',
+    name: 'Apollo Milton Obote',
+    startDate: '8 Sep 1967',
+    endDate: '25 Jan 1971',
+    term:'first'
+  },
+  {
+    title: 'His Excellency',
+    name: 'Idi Amin Dada Oumee',
+    startDate: '12 Mar 1971',
+    endDate: '11 Apr 1979',
+    term:'first'
+  },
+  {
+    title: 'His Excellency',
+    name: 'Yusufu Kironde Lule',
+    startDate: '13 Apr 1979',
+    endDate: '20 Jun 1979',
+    term:'first'
+  },
+  {
+    title: 'His Excellency',
+    name: 'Godfrey Lukwonga Binaisa',
+    startDate: '20 Jun 1979 ',
+    endDate: '12 May 1980',
+    term:'first'
+  },
+  {
+    title: 'His Excellency',
+    name: '	Apollo Milton Opeto Obote',
+    startDate: '15 Dec 1980',
+    endDate: '27 Jul 1985',
+    term:'second'
+  },
+  {
+    title: 'His Excellency',
+    name: 'Tito Okello Lutwa',
+    startDate: '29 Jul 1985',
+    endDate: '26 Jan 1986',
+    term:'first'
+  },
+  {
+    title: 'His Excellency',
+    name: 'Yoweri Kaguta Museveni',
+    startDate: '29 Jan 1986',
+    endDate: 'ongoing',
+    term:'current'
+  }
+]
